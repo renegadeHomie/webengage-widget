484c5a3ae5c7abdb2a47e0296414a21f38803a5f
farhan <farhan.ali@webklipper.com>
2020-08-07 19:32:12 +0530

    added unl sw

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,3 @@
-importScripts('https://ssl.widgets.webengage.com/js/service-worker.js');
+importScripts('https://widgets.unl.webengage.com/js/service-worker.js');
+
+// importScripts('https://ssl.widgets.webengage.com/js/service-worker.js');
